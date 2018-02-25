88ed4db0dbe74261fb415f24caa5235c529e95bb
Mahmoud SEHSAH <msehsah@d2-si.eu>
2018-02-25 13:47:14 +0100

    Adding Tutorial 1

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# List of Tutorial on Terraform
+
+* Tutorial 1 : Getting started with Terraform
---
+++ Tutorial 1/terraform.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  default = ""
+}
+
+variable "access_key" {
+  default = ""
+}
+
+variable "secret_key" {
+  default = ""
+}
+
